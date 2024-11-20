87c7cd3ba7c265569699910667a9015105e74729
unknown <nahidujjamanraj@gmail.com>
2024-11-20 20:25:09 +0600

    In this program I am dynamicly hidden my home page and show the keyboard page

--- javaScript/first.js
+++ javaScript/first.js
@@ -1,3 +1,12 @@
-function hidden(){
-    console.log('hidden the score page ')
+// function hidden_font_page(){
+//     let hidden_page=document.getElementById('menu_section')
+//     hidden_page.classList.add('hidden')
+
+//     let show_page=document.getElementById('keyboard_section')
+//     show_page.classList.remove('hidden')
+// }
+
+function hidden_font_page(){
+    utility('menu_section')
+    utility2('keyboard_section')
 }
---
+++ javaScript/utility.js
@@ -0,0 +1,9 @@
+function utility(elementId){
+    let hidden_page=document.getElementById(elementId)
+    hidden_page.classList.add('hidden')
+
+}
+function utility2(elementId2){
+    let show_page=document.getElementById(elementId2)
+    show_page.classList.remove('hidden')
+}
